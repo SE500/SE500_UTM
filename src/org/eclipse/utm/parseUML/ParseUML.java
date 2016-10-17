75eefb5195872c61a63f45e9ae81f0b13f1f1af1
stewmystre <tscolborne@outlook.com>
2016-10-17 16:20:26 -0400

    Including parseUML functionality

---
+++ src/org/eclipse/utm/parseUML/ParseUML.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * 		Thomas Colborne (ERAU) 
+ ******************************************************************************/
+package org.eclipse.utm.parseUML;
+
+//import org.eclipse.umlgen.gen.java.main.Uml2java;
+
+/**
+ * @author Thomas Colborne
+ *
+ */
+//@SuppressWarnings("restriction")
+public class ParseUML {
+	
+	/*
+	 * @constructor 
+	 * 
+	 */
+	public ParseUML() {
+		
+	}
+	
+}
