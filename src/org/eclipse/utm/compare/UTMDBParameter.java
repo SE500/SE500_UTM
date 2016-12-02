873dd14ea2132612e212f8ba23b7fc38ec7e9bb7
Michael Freudeman <michael.freudeman@gmail.com>
2016-12-02 16:59:33 -0500

    Added UTMDBParameter Class

---
+++ src/org/eclipse/utm/compare/UTMDBParameter.java
@@ -0,0 +1,9 @@
+package org.eclipse.utm.compare;
+
+public class UTMDBParameter {
+	public int ParameterIndex;
+	public int MethodID;
+	public int ClassID;
+	public String Type;
+	public String Name;
+}
