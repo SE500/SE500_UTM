fb2d422081b3fd60a750bf27d9e44d67beff562b
Michael Freudeman <michael.freudeman@gmail.com>
2016-12-02 18:58:24 -0500

    Updated Test Compare

--- src/org/eclipse/utm/test/TestCompare.java
+++ src/org/eclipse/utm/test/TestCompare.java
@@ -7,6 +7,7 @@ import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.utm.compare.UTMDB;
 import org.eclipse.utm.parseSource.ParseSource;
 import org.eclipse.utm.parseUML.ParseUML;
+import java.sql.*;
 /**
  * 
  * @author Thomas Colborne
@@ -18,41 +19,45 @@ public class TestCompare {
 	public static void main(String[] args) throws IOException {
 		boolean sourceParsed = false;
 		boolean umlParsed = false;
-		UTMDB db = null;
 		
-		File selectedSource = ParseSource.selectSource();
-		File selectedModel = ParseUML.selectUmlFile();
+//		File selectedSource = ParseSource.selectSource();
+//		File selectedModel = ParseUML.selectUmlFile();
+//		
+//		
+//		if(selectedSource != null) {
+//			ParseSource parseSource = new ParseSource(selectedSource);
+//			sourceParsed = parseSource.launch(new NullProgressMonitor());
+//		}
+//		else {
+//			System.err.println("Error: No Source File or Folder selected.");
+//		}
+//		if(selectedModel != null) {
+//			ParseUML parseUML = new ParseUML(selectedModel);
+//			umlParsed = parseUML.launch(new NullProgressMonitor(),true);
+//		}
+//		else {
+//			System.err.println("Error: No UML File selected.");
+//		}
 		
+		UTMDB db = new UTMDB();
+		if(!db.Open())
+		{
+			System.err.println("DB.Open Failed");
+		}
+		db.InitDatabase();
+		db.NewSourceReference("MyBaseClass", "extends", "MySuperClass");
+		db.NewSourceReference("MyBaseClass", "implements", "MyOtherClass");
+		db.NewSourceClass("Somefile.java", 1, "MyBaseClass", "public", false, false, false);
+		db.Relate();
+		db.Match();
+		db.Commit();
+		System.out.println("UML Class Count:\t" + db.CountUMLClasses());
+		System.out.println("Source Class Count:\t" + db.CountSourceClasses());
+		System.out.println("UML Attribute Count:\t" + db.CountUMLAttributes());
+		System.out.println("Source Attribute Count:\t" + db.CountSourceAttributes());
+		System.out.println("UML Method Count:\t" + db.CountUMLMethods());
+		System.out.println("Source Method Count:\t" + db.CountSourceMethods());
 		
-		if(selectedSource != null) {
-			ParseSource parseSource = new ParseSource(selectedSource);
-			sourceParsed = parseSource.launch(new NullProgressMonitor());
-		}
-		else {
-			System.err.println("Error: No Source File or Folder selected.");
-		}
-		if(selectedModel != null) {
-			ParseUML parseUML = new ParseUML(selectedModel);
-			umlParsed = parseUML.launch(new NullProgressMonitor(),true);
-		}
-		else {
-			System.err.println("Error: No UML File selected.");
-		}
-		
-		if(sourceParsed && umlParsed) {
-			db = new UTMDB();
-			db.Open();
-			db.InitDatabase();
-			db.Relate();
-			db.Match();
-			db.Commit();
-			System.out.println("UML Class Count:\t" + db.CountUMLClasses());
-			System.out.println("Source Class Count:\t" + db.CountSourceClasses());
-			System.out.println("UML Attribute Count:\t" + db.CountUMLAttributes());
-			System.out.println("Source Attribute Count:\t" + db.CountSourceAttributes());
-			System.out.println("UML Method Count:\t" + db.CountUMLMethods());
-			System.out.println("Source Method Count:\t" + db.CountSourceMethods());
-		}
 		System.exit(0);
 	}
 }
